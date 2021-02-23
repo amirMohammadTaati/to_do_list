8e6b57b4075d91b2b0b210fccea93afc6bcd367d
amirmohammad <mamad198585@gmail.com>
2021-02-23 16:16:32 +0330

    render() lifecycle added

--- main/src/App.js
+++ main/src/App.js
@@ -7,7 +7,26 @@ class App extends React.Component{
         this.input = React.createRef();
         this.handleClick = this.handleClick.bind(this);
         this.done        = this.done.bind(this);
-      }
+    }
+
+    render(){
+        return(
+            <div className="container">
+              <div className="input-section">
+                <input 
+                type="text" 
+                placeholder="New todo..."
+                ref={this.input}
+                />
+                <span onClick={this.handleClick}>+</span>
+              </div>
+     
+              <div className="to-do-list">
+                {cards}
+              </div>
+            </div>
+         );
+    }
     
 }
 
